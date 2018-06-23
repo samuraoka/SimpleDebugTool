1ea8736d9d5ae7c792dc14c380695df9fb9dc650
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-24 08:56:26 +0900

    Implemented some code.
    unique_handle class:
    removed copy constructor.
    removed copy assignment operator.
    added move constructor.
    added move assignment operator.
    added release member function.
    added reset member function.

--- SimpleDebugTool/SimpleDebugTool/handle.h
+++ SimpleDebugTool/SimpleDebugTool/handle.h
@@ -21,14 +21,35 @@ namespace KennyKerr
 		}
 
 	public:
-		//TODO
 
+		// Meaning of = delete after function declaration
+		// https://stackoverflow.com/questions/5513881/meaning-of-delete-after-function-declaration
+		unique_handle(unique_handle const &) = delete;
+		auto operator=(unique_handle const &)->unique_handle & = delete;
+
+		// Throw keyword in function's signature
+		// https://stackoverflow.com/questions/1055387/throw-keyword-in-functions-signature
 		explicit unique_handle(pointer value = Traits::invalid()) throw() :
 			m_value{ value }
 		{
 		}
 
-		//TODO
+		// Move Constructors and Move Assignment Operators (C++)
+		// https://docs.microsoft.com/en-us/cpp/cpp/move-constructors-and-move-assignment-operators-cpp
+		unique_handle(unique_handle && other) throw() :
+			m_value{ other.release() }
+		{
+		}
+
+		auto operator=(unique_handle && other) throw() -> unique_handle &
+		{
+			if (this != &other)
+			{
+				reset(other.release());
+			}
+
+			return *this;
+		}
 
 		~unique_handle() throw()
 		{
@@ -43,6 +64,24 @@ namespace KennyKerr
 		auto get() const throw() -> pointer
 		{
 			return m_value;
+		}
+
+		auto release() throw() -> pointer
+		{
+			auto value = m_value;
+			m_value = Traits::invalid();
+			return value;
+		}
+
+		auto reset(pointer value = Traits::invalid()) throw() -> bool
+		{
+			if (m_value != value)
+			{
+				close();
+				m_value = value;
+			}
+
+			return static_cast<bool>(*this);
 		}
 
 		//TODO
