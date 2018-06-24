e831add92781c9712a600231b5f2b66001c79aa6
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-24 13:52:36 +0900

    Added variadic templates repeal function.

--- SimpleDebugTool/SimpleDebugTool/util.h
+++ SimpleDebugTool/SimpleDebugTool/util.h
@@ -19,4 +19,26 @@ namespace KennyKerr
 
 	auto SIMPLEDEBUGTOOL_API wait_one(
 		HANDLE const h, DWORD const milliseconds = INFINITE) -> bool;
+
+	//auto pack(HANDLE *) -> void {}
+
+	template <typename T, typename... Args>
+	auto pack(HANDLE * left, T const & right, Args const & ... args) -> void
+	{
+		*left = right.get();
+		pack(++left, args...);
+	}
+
+	template <typename... Args>
+	auto wait_all(Args const & ... args) -> DWORD
+	{
+		HANDLE handles[sizeof...(Args)];
+		pack(handles, args...);
+
+		auto ret = WaitForMultipleObjects(
+			sizeof...(Args), handles, true, INFINITE);
+		VERIFY_(WAIT_OBJECT_0, ret);
+
+		return ret;
+	}
 }
