aa28b900e537c0697ea1b13fc346e19f047c4f3e
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-24 10:12:47 +0900

    Implemented event class.

--- SimpleDebugTool/SimpleDebugTool/event.cpp
+++ SimpleDebugTool/SimpleDebugTool/event.cpp
@@ -1,1 +1,47 @@
 #include "stdafx.h"
+#include "event.h"
+
+using KennyKerr::event_type;
+using KennyKerr::event;
+
+event::event(event_type type) :
+	h{ CreateEvent(nullptr, static_cast<BOOL>(type), false, nullptr) }
+{
+	if (!h) throw windows_exception();
+}
+
+event::event(event && other) throw() :
+	h{ other.h.release() }
+{
+}
+
+auto event::operator=(event && other) throw()->event &
+{
+	// std::move
+	// https://en.cppreference.com/w/cpp/utility/move
+	h = std::move(other.h);
+	return *this;
+}
+
+auto event::set() throw() -> void
+{
+	VERIFY(SetEvent(h.get()));
+}
+
+auto event::clear() throw() -> void
+{
+	VERIFY(ResetEvent(h.get()));
+}
+
+auto event::wait(DWORD const milliseconds) throw() -> bool
+{
+	auto const result = WaitForSingleObject(h.get(), milliseconds);
+	ASSERT(result == WAIT_OBJECT_0 || result == WAIT_TIMEOUT);
+
+	return result == WAIT_OBJECT_0;
+}
+
+auto event::get() const throw() -> HANDLE
+{
+	return h.get();
+}
--- SimpleDebugTool/SimpleDebugTool/event.h
+++ SimpleDebugTool/SimpleDebugTool/event.h
@@ -1,1 +1,48 @@
 #pragma once
+
+#include <utility>
+#include "handle.h"
+#include "util.h"
+
+#ifdef SIMPLEDEBUGTOOL_EXPORTS
+#define SIMPLEDEBUGTOOL_API __declspec(dllexport)
+#else
+#define SIMPLEDEBUGTOOL_API __declspec(dllimport)
+#endif
+
+namespace KennyKerr
+{
+	enum class event_type
+	{
+		auto_reset,
+		manual_reset,
+	};
+
+	class SIMPLEDEBUGTOOL_API event
+	{
+		null_handle h;
+
+	public:
+		// Meaning of = delete after function declaration
+		// https://stackoverflow.com/questions/5513881/meaning-of-delete-after-function-declaration
+		event(event const &) = delete;
+		auto operator=(event const &)->event & = delete;
+		~event() = default;
+
+		explicit event(event_type type);
+
+		// Move Constructors and Move Assignment Operators (C++)
+		// https://docs.microsoft.com/en-us/cpp/cpp/move-constructors-and-move-assignment-operators-cpp
+		// 
+		// Throw keyword in function's signature
+		// https://stackoverflow.com/questions/1055387/throw-keyword-in-functions-signature
+		event(event && other) throw();
+
+		auto operator=(event && other) throw()->event &;
+
+		auto set() throw() -> void;
+		auto clear() throw() -> void;
+		auto wait(DWORD const milliseconds = INFINITE) throw() -> bool;
+		auto get() const throw()->HANDLE;
+	};
+}
