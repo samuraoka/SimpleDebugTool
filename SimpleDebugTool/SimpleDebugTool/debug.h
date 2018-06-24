e4162a9ae66c43590bfd0f9222683e2b75f3dd2d
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-24 14:29:42 +0900

    Modified format.

--- SimpleDebugTool/SimpleDebugTool/debug.h
+++ SimpleDebugTool/SimpleDebugTool/debug.h
@@ -38,14 +38,17 @@ namespace KennyKerr
 		Tracer(char const *filename, unsigned const line);
 
 		template <typename... Args>
-		auto operator()(wchar_t const *format, Args... args) const -> void
+		auto operator()(
+			wchar_t const *format, Args... args) const -> void
 		{
 			wchar_t buffer[1024];
-			auto count = swprintf_s(buffer, L"[%d] %S(%d): ", m_pid, m_filename, m_line);
+			auto count = swprintf_s(
+				buffer, L"[%d] %S(%d): ", m_pid, m_filename, m_line);
 			ASSERT(-1 != count);
 
-			ASSERT(-1 != _snwprintf_s(buffer + count, _countof(buffer) - count,
-				_countof(buffer) - count - 1, format, args...));
+			ASSERT(-1 != _snwprintf_s(buffer + count,
+				_countof(buffer) - count, _countof(buffer) - count - 1,
+				format, args...));
 
 			OutputDebugString(buffer);
 		}
